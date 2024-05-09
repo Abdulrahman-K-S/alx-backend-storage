a749924bc553a56ff91f963614de657375525297
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-05-10 01:30:38 +0300

    Added task "4. Buy buy buy"
    Write a SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
    
    Quantity in the table `items` can be negative.
    
    **Context:** *Updating multiple tables for one action from your application can generate issue: network disconnection, crash, etc… to keep your data in a good shape, let MySQL do it for you!*
    
    ```sql
    bob@dylan:~$ cat 4-init.sql
    -- Initial
    DROP TABLE IF EXISTS items;
    DROP TABLE IF EXISTS orders;
    
    CREATE TABLE IF NOT EXISTS items (
        name VARCHAR(255) NOT NULL,
        quantity int NOT NULL DEFAULT 10
    );
    
    CREATE TABLE IF NOT EXISTS orders (
        item_name VARCHAR(255) NOT NULL,
        number int NOT NULL
    );
    
    INSERT INTO items (name) VALUES ("apple"), ("pineapple"), ("pear");
    
    bob@dylan:~$
    bob@dylan:~$ cat 4-init.sql | mysql -uroot -p holberton
    Enter password:
    bob@dylan:~$
    bob@dylan:~$ cat 4-store.sql | mysql -uroot -p holberton
    Enter password:
    bob@dylan:~$
    bob@dylan:~$ cat 4-main.sql
    Enter password:
    -- Show and add orders
    SELECT * FROM items;
    SELECT * FROM orders;
    
    INSERT INTO orders (item_name, number) VALUES ('apple', 1);
    INSERT INTO orders (item_name, number) VALUES ('apple', 3);
    INSERT INTO orders (item_name, number) VALUES ('pear', 2);
    
    SELECT "--";
    
    SELECT * FROM items;
    SELECT * FROM orders;
    
    bob@dylan:~$
    bob@dylan:~$ cat 4-main.sql | mysql -uroot -p holberton
    Enter password:
    name    quantity
    apple   10
    pineapple   10
    pear    10
    --
    --
    name    quantity
    apple   6
    pineapple   10
    pear    8
    item_name   number
    apple   1
    apple   3
    pear    2
    bob@dylan:~$
    ```
    
    Files:
    - 4-store.sql

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,8 @@
+-- Task 4. Buy buy buy
+-- A trigger that decreases the quantity of an item after adding a new order.
+CREATE TRIGGER decrease_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
