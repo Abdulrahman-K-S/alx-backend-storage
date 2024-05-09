49679c6895a9ef2098eea53f12f7208366fdf074
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-05-10 01:33:27 +0300

    Added task "5. Email validation to sent"
    Write a SQL script that creates a trigger that resets the attribute `valid_email` only when the `email` has been changed.
    
    **Context:** *Nothing related to MySQL, but perfect for user email validation - distribute the logic to the database itself!*
    
    ```sql
    bob@dylan:~$ cat 5-init.sql
    -- Initial
    DROP TABLE IF EXISTS users;
    
    CREATE TABLE IF NOT EXISTS users (
        id int not null AUTO_INCREMENT,
        email varchar(255) not null,
        name varchar(255),
        valid_email boolean not null default 0,
        PRIMARY KEY (id)
    );
    
    INSERT INTO users (email, name) VALUES ("bob@dylan.com", "Bob");
    INSERT INTO users (email, name, valid_email) VALUES ("sylvie@dylan.com", "Sylvie", 1);
    INSERT INTO users (email, name, valid_email) VALUES ("jeanne@dylan.com", "Jeanne", 1);
    
    bob@dylan:~$
    bob@dylan:~$ cat 5-init.sql | mysql -uroot -p holberton
    Enter password:
    bob@dylan:~$
    bob@dylan:~$ cat 5-valid_email.sql | mysql -uroot -p holberton
    Enter password:
    bob@dylan:~$
    bob@dylan:~$ cat 5-main.sql
    Enter password:
    -- Show users and update (or not) email
    SELECT * FROM users;
    
    UPDATE users SET valid_email = 1 WHERE email = "bob@dylan.com";
    UPDATE users SET email = "sylvie+new@dylan.com" WHERE email = "sylvie@dylan.com";
    UPDATE users SET name = "Jannis" WHERE email = "jeanne@dylan.com";
    
    SELECT "--";
    SELECT * FROM users;
    
    UPDATE users SET email = "bob@dylan.com" WHERE email = "bob@dylan.com";
    
    SELECT "--";
    SELECT * FROM users;
    
    bob@dylan:~$
    bob@dylan:~$ cat 5-main.sql | mysql -uroot -p holberton
    Enter password:
    id  email   name    valid_email
    1   bob@dylan.com   Bob 0
    2   sylvie@dylan.com    Sylvie  1
    3   jeanne@dylan.com    Jeanne  1
    --
    --
    id  email   name    valid_email
    1   bob@dylan.com   Bob 1
    2   sylvie+new@dylan.com    Sylvie  0
    3   jeanne@dylan.com    Jannis  1
    --
    --
    id  email   name    valid_email
    1   bob@dylan.com   Bob 1
    2   sylvie+new@dylan.com    Sylvie  0
    3   jeanne@dylan.com    Jannis  1
    bob@dylan:~$
    ```
    
    Files:
    - 5-valid_email.sql

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,12 @@
+-- Task 5. Email validation to sent
+-- A trigger that resets the attribute `valid_email` only when the `email` has been changed.
+DELIMITER $$
+CREATE TRIGGER new_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email THEN
+	SET NEW.valid_email = 0;
+    END IF;
+END$$
+DELIMITER ;$$
