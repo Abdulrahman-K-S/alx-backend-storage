8cd214d20340ac39995717ebd5f0ea7d0baad4dd
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-05-15 23:17:09 +0300

    Added task "1. Reading from Redis and recovering original type"
    Redis only allows to store string, bytes and numbers (and lists thereof). Whatever you store as single elements, it will be returned as a byte string. Hence if you store `"a"` as a UTF-8 string, it will be returned as `b"a"` when retrieved from the server.
    
    In this exercise we will create a `get` method that take a `key` string argument and an optional `Callable` argument named `fn`. This callable will be used to convert the data back to the desired format.
    
    Remember to conserve the original `Redis.get` behavior if the key does not exist.
    
    Also, implement 2 new methods: `get_str and `get_int` that will automatically parametrize `Cache.get` with the correct conversion function.
    
    The following code should not raise:
    
    ```py
    cache = Cache()
    
    TEST_CASES = {
        b"foo": None,
        123: int,
        "bar": lambda d: d.decode("utf-8")
    }
    
    for value, fn in TEST_CASES.items():
        key = cache.store(value)
        assert cache.get(key, fn=fn) == value
    ```
    
    Files:
    - exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,7 +7,7 @@ Create a cache class and create it's constructor and store method.
 
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -38,3 +38,52 @@ class Cache:
         redisKey = str(uuid4())
         self._redis.set(redisKey, data)
         return redisKey
+
+    def get(self, key: str, fn: Callable = None):
+        """get
+
+        This method takes in a key str and an optional callable and returns
+        the data with the correct conversion function.
+
+        Arguments:
+            key (str): The key which we'll get its data
+            fn (Callable): The function we'll invoke. Default=None
+
+        Return:
+            (str/int): Depending on what the fn will be.
+        """
+        data = self._redis.get(key)
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """get_str
+
+        The str conversion function
+
+        Arguments:
+            key (str): The key which we'll get its data
+
+        Return:
+            (str): The data as an str variable
+        """
+        data = self._redis.get(key)
+        return data.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """get_int
+
+        The int conversion function
+
+        Arguments:
+            key (str): The key which we'll get its data
+
+        Return:
+            (int): The data as an int variable
+        """
+        try:
+            data = int((self._redis.get(key)).decode("utf-8"))
+        except Exception:
+            data = 0
+        return data
