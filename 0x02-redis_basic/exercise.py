608870ebb769a40ccf72408cbf82b1a890e40db5
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-05-15 22:52:40 +0300

    Added task "0. Writing strings to Redis"
    Create a `Cache` class. In the `__init__` method, store an instance of the Redis client as a private variable named `_redis` (using `redis.Redis()`) and flush the instance using `flushdb`.
    
    Create a `store` method that takes a `data` argument and returns a string. The method should generate a random key (e.g. using `uuid`), store the input data in Redis using the random key and return the key.
    
    Type-annotate `store` correctly. Remember that data can be a `str`, `bytes`, `int` or `float`.
    
    bob@dylan:~$ cat main.py
    #!/usr/bin/env python3
    """
    Main file
    """
    import redis
    
    Cache = __import__('exercise').Cache
    
    cache = Cache()
    
    data = b"hello"
    key = cache.store(data)
    print(key)
    
    local_redis = redis.Redis()
    print(local_redis.get(key))
    
    bob@dylan:~$ python3 main.py
    3a3e8231-b2f6-450d-8b0e-0f38f16e8ca2
    b'hello'
    bob@dylan:~$
    ```
    
    Files:
    - exercise.py

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Task 0. Writing strings to Redis
+
+Create a cache class and create it's constructor and store method.
+"""
+
+import redis
+from uuid import uuid4
+from typing import Union
+
+
+class Cache:
+    """Cache
+    """
+    def __init__(self):
+        """__init__
+
+        The initalization of the Cache class stores an instance of redis
+        in the private variable _redis and flushes the instance using flushdb
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """store
+
+        This method generates a random key, stores the input data in Redis using
+        the random key and returns the key.
+
+        Arguments
+            data ():
+
+        Return:
+            (str): The random key generated.
+        """
+        redisKey = str(uuid4())
+        self._redis.set(redisKey, redisKey)
+        return redisKey
